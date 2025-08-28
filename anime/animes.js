9cd8976b80702482600846b7c79b960e15e1e6ed
User12351bs <lutukisolo@gmail.com>
2025-08-28 18:26:36 +0200

    Update animes.js

--- anime/animes.js
+++ anime/animes.js
@@ -11,7 +11,7 @@ const animeData = [
         logo: "https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=600/keyart/GRE50KV36-title_logo-en-us",
         inMyList: false,
         subtitled: true,
-        dubbed: false,
+        dubbed: true,
         contentRating: "+14",
         contentWarning: "",
         description: "En un mundo en el que la magia lo es todo, Asta y Yuno son dos niños que encuentran abandonados el mismo día en una iglesia. Mientras que Yuno crece demostrando tener unos grandes poderes mágicos, Asta parece ser la única persona en el mundo que no posee capacidad mágica alguna. Eso no importará a los dos jóvenes, que aspirarán a alcanzar el puesto de Rey Mago, el mejor mago de todos. Pero para ello tendrán que ser seleccionados primero por una Orden de Caballeros Mágicos, ¡y Asta no tiene magia!",
@@ -59,7 +59,29 @@ const animeData = [
             }
         ],
         generatedDate: "2025-08-27T11:04:12.494Z"
-    }
+    },
+    {
+        id: 3,
+        title: "Ouran High School Host Club",
+        image: "https://m.media-amazon.com/images/M/MV5BMGMzZTQyMmUtNWUwZS00NDU2LWE1Y2YtZWVkNGM1MGNkZDFjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        banner: "https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=cover,format=auto,quality=85,width=1920/keyart/GRGGJWD2R-backdrop_wide",
+        logo: "https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=600/keyart/GRGGJWD2R-title_logo-en-us",
+        inMyList: false,
+        subtitled: true,
+        dubbed: false,
+        contentRating: "+14",
+        contentWarning: "",
+        description: "Te enamorarás de los chicos del Club de Anfitriones de Ouran: Tamaki es un auténtico romántico. Kaoru y Hikaru te ofrecen muestras de amor fraternal. Adorarás al inteligente Kyoya, a la inocente Honey y al varonil Mori. Ah, y no te olvides de Haruhi. Él sabe lo que quieren las chicas; claro, ¡es porque él también es una chica! Los anfitriones deben hacer todo lo posible por ocultar su secreto, pero Haruhi se lo pasa bomba poniendo patas arriba su mundo perfecto.",
+        seasons: [
+            {
+                number: 1,
+                title: "Temporada 1",
+                episodes: 26,
+                slug: "ouran-host-club"
+            }
+        ],
+        generatedDate: "2025-08-28T16:25:21.872Z"
+    },
 ];
 
 // Configuración global para todos los animes
